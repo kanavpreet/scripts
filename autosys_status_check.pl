bfb86913c882f50478407a908ea66d28603ffd75
Kanav Preet <30400257+kanavpreet@users.noreply.github.com>
2020-01-05 12:18:36 +0530

    Update and rename autosys_status_check to autosys_status_check.pl

--- autosys_status_check.pl
+++ autosys_status_check.pl
@@ -11,7 +11,21 @@ foreach $arg(0....$#argv)
 {
 $Email_grp= $Email_grp.",".$argv[$arg];
 }
-my @Arraylist= autorep -J <regex> -q | grep "days" -B 6| grep <reqex>
+my @Arraylist= 'autorep -J <regex> -q | grep "days" -B 6| grep <reqex>';
 
 ##Filtering autosys job result using grep and awk ##
 ## Autosys - autorep -J will fetch Job details and -q will extract value from its Jil
+
+foreach(@Arraylist)
+{
+chomp($_);
+my $date2= 'autorep -j $_ -t US/EASTERN | awk '{if(\$0 ~ /^ $_/{print <value>})}'';
+chomp($date2);
+push($date3,$date2);
+
+}
+
+sub SendMail()
+{
+  my $msg=MIME::Lite->new 
+}
